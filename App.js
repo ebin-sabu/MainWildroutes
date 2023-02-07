076bfdbe548734f382020c9698c38497dcd66ce7
Ebin Pereppadan Sabu <ebin.pereppadan01@gmail.com>
2023-02-07 16:17:44 +0000

    Starter

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { 
+  SafeAreaView, 
+  StyleSheet, 
+  Text, 
+  Image,
+  Alert,
+  TouchableOpacity, 
+} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style ={styles.Title}>WildRoutes</Text>
+      <Image style={styles.Logo} source={
+        require('./assets/MainIcon.png')}/>
+      <TouchableOpacity style={styles.button} onPress={() => Alert.alert('Coming Soon!', 'This App is Currently in Development')}>
+        <Text style={styles.buttonText}>Sign Up</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#e3dcc5',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  Logo: {
+    width: 300,
+    height: 300,
+  },
+  Title: {
+    color: '#013220',
+    fontFamily:'Papyrus',
+    fontWeight: 'bold',
+    fontSize: 40,
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: '#004300',
+  },
+  buttonText: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
   },
 });
